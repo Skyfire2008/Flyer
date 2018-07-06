d7e6c942d445ffa6ee4ca3052a1b95c9f93fd18d
Leonid Darovskikh <skyfire2008@gmail.com>
2018-07-06 23:24:40 +0200

    created project

---
+++ src/org/skyfire2008/flyer/Main.hx
@@ -0,0 +1,22 @@
+package org.skyfire2008.flyer;
+
+import flash.display.StageAlign;
+import flash.display.StageScaleMode;
+import flash.Lib;
+
+/**
+ * ...
+ * @author 
+ */
+class Main 
+{
+	
+	static function main() 
+	{
+		var stage = Lib.current.stage;
+		stage.scaleMode = StageScaleMode.NO_SCALE;
+		stage.align = StageAlign.TOP_LEFT;
+		// Entry point
+	}
+	
+}
